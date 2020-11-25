0174058929c82151167488c9713900cfa611fe3f
Neill Bogie <neillbogie@googlemail.com>
2020-11-25 17:59:09 +0000

    proportional disturbance to movement

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@
 //this variable will hold our shader object
 
 let myShader;
+
+//a disturbance, 0-1 which we pass to the shader to affect amplitude of oscillation
+//Increases with user activity, and decays over time.
 let disturbance = 0.0;
 
 function preload() {
@@ -18,21 +21,23 @@ function setup() {
   // shaders require WEBGL mode to work
   createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
+  mouseX = width / 2;
+  mouseY = height / 2;
+
 }
-function mouseMoved() {
-  disturbance = min(1.0, disturbance + 0.03);
-}
+
 function draw() {
   background(0);
-  orbitControl(1, 1, 0.2);
+  orbitControl(1, 1, 0.02);
   // shader() sets the active shader with our shader
   shader(myShader);
 
   // Send the frameCount to the shader
   myShader.setUniform("uFrameCount", frameCount);
+  //Send a disturbance value (0-1) to the shader (we decay this each frame)
   myShader.setUniform("uDisturbance", disturbance);
 
-  // Rotate our geometry on the X and Y axes
+  // Animate rotate of our geometry on the X and Y axes
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.005);
 
@@ -40,7 +45,16 @@ function draw() {
   // We're going to tessellate the sphere a bit so we have some more geometry to work with
   const detail = 200;
   sphere(width / 5, detail, detail);
+  //Decay the disturbance a little so that it calms over time
   disturbance = max(0.0, disturbance - 0.003);
+}
+
+function mouseMoved() {
+  //increase disturbance when the mouse is moved, proportional to the size of movement.
+  const dst = dist(pmouseX, pmouseY, mouseX, mouseY);
+  const minDim = min(width, height);
+  const moveFrac = dst / minDim; //last frame's mouse movement as fraction of smaller screen dimension.
+  disturbance = min(1.0, disturbance + moveFrac / 5);
 }
 
 function windowResized() {
